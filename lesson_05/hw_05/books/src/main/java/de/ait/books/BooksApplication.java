34b8e9e496718eb7b9e0ba28950a431fa138d294
Liubov Korchashkina <korchashkina.liubov@gmail.com>
2024-08-14 09:35:32 +0200

    add task

--- lesson_05/hw_05/books/src/main/java/de/ait/books/BooksApplication.java
+++ lesson_05/hw_05/books/src/main/java/de/ait/books/BooksApplication.java
@@ -3,6 +3,16 @@ package de.ait.books;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+Выберете любую сущность, например Book.
+Создайте приложение, используя https://start.spring.io/  Подключите зависимость Spring Web
+По примеру написанному на занятие, создайте слои приложения, и реализуйте:
+1) получение всех (книг) при get запросе на /books
+2) добавление книги по запросу post
+
+ */
+
+
 @SpringBootApplication
 public class BooksApplication {
 
